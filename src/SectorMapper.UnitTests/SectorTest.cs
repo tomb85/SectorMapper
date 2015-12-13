fc6affad1974a948c1d87fc6608f4649ce1aadfd
FollowFurion <followfurion@gmail.com>
2015-12-13 18:17:22 +0100

    added Sektor IMPLEMENTEJSZYN

--- src/SectorMapper.UnitTests/SectorTest.cs
+++ src/SectorMapper.UnitTests/SectorTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace SectorMapper.UnitTests
 {
@@ -7,9 +8,71 @@ namespace SectorMapper.UnitTests
     public class SectorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConstructorShouldReturnGoodArguments()
         {
-            // TODO
+            var testowysector = new Sector(12,10,4,6); //var gwarantuje, ze stworzony obiekt bedzie tego samego typu, co obiekt stworzony rpze wywolanie klasy (new)
+                // nazwaobiektu  new<- zawsze   Sector() czyli z czego ma go budowac          
+
+            Assert.AreEqual(12, testowysector.Id, "Id doesn't match"); //Assert = wbudowana klasa, sluzaca do porownywania czy wartosci zostaly poprawnie zainicjalizowane
+            Assert.AreEqual(10, testowysector.FillTreshhold, "FT doesn't match");
+            Assert.AreEqual(4, testowysector.Width, "W doesn't match");
+            Assert.AreEqual(6, testowysector.Height, "H doesn't match");
+        }
+
+        [TestMethod]
+        public void CzekErja()
+        {
+            var testowysector = new Sector(fillTreshhold: 1, id: 1, height: 5, width: 10); // <-- to jest wzorcowy model wywoalania funkcji, nie wkladam po kolei 4x random numer, bo nie wiadomo o co chodzi, tylko po kolei nazywasz i przypisujesz wartosc, ze inna osoba wie o co kaman
+            Assert.AreEqual(50, testowysector.Area, "area doesn't match");
+        }
+
+        [TestMethod]
+        public void CzekFillCountIsInnitializedToZerou()
+        {
+            var testowysecotr = new Sector(fillTreshhold: 1, id: 1, height: 5, width: 10);
+            Assert.AreEqual(0, testowysecotr.FillCount, "hahaha nie");
+        }
+        
+        [TestMethod]
+        public void CheckIncreaseFillCount()
+        {
+            var testowysec = new Sector(fillTreshhold: 1, id: 1, height: 5, width: 10);
+            testowysec.IncreaseFillCount();
+            testowysec.IncreaseFillCount();
+            Assert.AreEqual(2, testowysec.FillCount, "should be 2, Jim");
+        }
+        [TestMethod]
+        public void TestCzyFillRatioPoprawnieDajeDouble()
+        {
+            var sut = new Sector(fillTreshhold: 1, id: 1, height: 5, width: 10);
+            for (int i =0;i<11;i++)
+            {
+                sut.IncreaseFillCount();
+            }
+            Assert.AreEqual(11, sut.FillCount, "Fill count is not 11");
+            Assert.AreEqual(0.22, sut.FillRatio, "Fill count rato of 11/50 = 0.22");
+        }
+
+        [TestMethod]
+        public void IsBlackDlaCzarnego()
+        {
+            var sut = new Sector(fillTreshhold: 0.5, id: 1, height: 5, width: 10);
+            for (int i = 0; i < 27; i++)
+            {
+                sut.IncreaseFillCount();
+            }
+            Assert.IsTrue(sut.IsBlack(), "spodziewalem sie czarnucha, a jest bialas");
+        }
+
+        [TestMethod]
+        public void IsBlackDlaBialego()
+        {
+            var sut = new Sector(fillTreshhold: 0.5, id: 1, height: 5, width: 10);
+            for (int i = 0; i < 17; i++)
+            {
+                sut.IncreaseFillCount();
+            }
+            Assert.IsFalse(sut.IsBlack(), "spodziewalem sie bialasa, a jest NEGROID, FillRatio: "+sut.FillRatio );
         }
     }
 }
--- src/SectorMapper/Sector.cs
+++ src/SectorMapper/Sector.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace SectorMapper
 {
-    class Sector
+    public class Sector
     {
+        public int Id { get; private set; } //jest private, czyli jest to klasa tylko do odczytu, jedyna mozliwosc ustalenia wartosci = w konstrucktorze
+        public double FillTreshhold { get; private set; } //odpowiednikiem w c++ byloby public getter + private setter + private property
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Area { get { return Width * Height; } }
+        public int FillCount { get; private set; } //uzywamy get private set aby "odizolowac" te wartosci od mozliwosci zmiany w innych czesciach programu
+        public double FillRatio { get { return FillCount / (double)Area; } } // area i fill count to int, wiec kompilator autoamtycznie zaklada ze wynik operacji to tez jest int; czyli 11/50 = 0 a nie 0.22. Zeby wyswietlal wynik jako double, to co najmniej 1 argument musi byc double, i to robimy narzucajc typ wartosci (double)nazwazmiennej
+
+        //----*---- konstruktor
+        public Sector(int id, double fillTreshhold, int width, int height) 
+        {
+            Id = id;
+            FillTreshhold = fillTreshhold;
+            Width = width;
+            Height = height;
+        }
+
+        public void IncreaseFillCount()
+        {
+            FillCount++;
+        }
+
+
+        public bool IsBlack()
+        {
+            return FillRatio > FillTreshhold;
+        }
     }
+    
 }
