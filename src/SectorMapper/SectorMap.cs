b448cf9d18b05c1b5491d70c761fbfbdfe3986ac
tomb85 <tom.b1985@yahoo.co.uk>
2015-12-13 11:58:35 +0000

    setup

--- src/SectorMapper/SectorMap.cs
+++ src/SectorMapper/SectorMap.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace SectorMapper
 {
-    class Program
+    class SectorMap
     {
-        static void Main(string[] args)
-        {
-            // TODOfdsfds
-        }
     }
 }
