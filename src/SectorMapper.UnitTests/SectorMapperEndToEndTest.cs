c6e4fe5f4d618421e6fb1d2389063953c01fadf8
tomb85 <tom.b1985@yahoo.co.uk>
2016-01-17 09:28:58 +0000

    update

---
+++ src/SectorMapper.UnitTests/SectorMapperEndToEndTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SectorMapper.UnitTests
+{
+    [TestClass]
+    public class SectorMapperEndToEndTest
+    {
+        [TestMethod]
+        public void ShouldProduceCorrectPathFile()
+        {
+            var mapper = new SectorMapperBuilder().
+
+        }
+    }
+}
