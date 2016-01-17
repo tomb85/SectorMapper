1d96697a93b582ce9995f84882f931f6937aa90a
FollowFurion <followfurion@gmail.com>
2016-01-17 18:36:16 +0100

    kolejny update po dodaniu nowego pliki BitMapLoader + Test

---
+++ src/SectorMapper.UnitTests/BitMapLoaderTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace SectorMapper.UnitTests
+{
+    [TestClass]
+    public class BitMapLoaderTest
+    {
+
+        private const int CORRECT_HEIGHT = 500;
+        private const int CORRECT_WIDTH = 500;
+
+        [TestMethod]
+        public void ShouldLoad1bppBitmapWithCorrectHeightAndWidth()
+        {
+            var loader = new FixedSizeBitMapLoader(CORRECT_WIDTH, CORRECT_HEIGHT);
+            var loadedbitmap = loader.LoadBitmap("Data/input_img_0.bmp");
+
+            Assert.AreEqual(CORRECT_HEIGHT, loadedbitmap.Height, "Height is not right");
+            Assert.AreEqual(CORRECT_WIDTH, loadedbitmap.Width, "Width is not right");
+            Assert.AreEqual(PixelFormat.Format1bppIndexed, loadedbitmap.PixelFormat, "Pixel format is " + loadedbitmap.PixelFormat + " and should be 1");
+
+
+
+        }
+    }
+}
---
+++ src/SectorMapper/BitMapLoader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace SectorMapper
+{
+    interface IBitMapLoader
+    {
+        Bitmap LoadBitmap(string path);
+    }
+
+    public class FixedSizeBitMapLoader : IBitMapLoader
+    {
+        public int ExpectedWidth {get; private set;}
+        public int ExpectedHeight { get; private set; }
+
+        public FixedSizeBitMapLoader(int width, int height)
+        {
+            ExpectedWidth = width;
+            ExpectedHeight = height;
+        }
+
+
+        public Bitmap LoadBitmap(string path)
+        {
+            var loadedbitmap = new Bitmap(path);
+            if (loadedbitmap.PixelFormat != PixelFormat.Format1bppIndexed)
+            {
+                throw new InvalidOperationException("Expected image w/ 1bpp, got image w/ " + PixelFormat.Format1bppIndexed + " bpp");  
+            }
+
+            if (loadedbitmap.Width != ExpectedWidth)
+            {
+                throw new InvalidOperationException("Expected image width should be "+ ExpectedWidth + " got image with" + loadedbitmap.Width);  
+            }
+
+            if (loadedbitmap.Height != ExpectedHeight)
+            {
+                throw new InvalidOperationException("Expected image Height should be " + ExpectedHeight + " got image with" + loadedbitmap.Height);
+            }
+
+            return loadedbitmap;
+        }
+
+
+    }
+
+}
